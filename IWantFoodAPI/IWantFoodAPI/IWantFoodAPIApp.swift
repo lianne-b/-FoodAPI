cb6d2120282a4f0d06604328798ba9c5159d32c0
lianne-b <yenchoi010@gmail.com>
2023-11-19 22:18:04 +0900

    initial commit

---
+++ IWantFoodAPI/IWantFoodAPI/IWantFoodAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IWantFoodAPIApp.swift
+//  IWantFoodAPI
+//
+//  Created by Ye Eun Choi on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct IWantFoodAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
